cdd2ee47ae2144666d3cb8053a3141bb09a25bf1
CaroMusangi1 <kitongacarol8@gmail.com>
2024-07-15 22:09:20 +0000

    organize me

--- frontend/tests/jest.js
+++ frontend/tests/jest.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders Music Recommender title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Music Recommender/i)/;
+  const linkElement = screen.getByText(/Music Recommender/i);
   expect(linkElement).toBeInTheDocument();
 });
